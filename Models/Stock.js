a41dd940c0c8cfa6798b94fcd25c47f788afe1fc
Mark Goldstein <markgoldstein12@gmail.com>
2019-10-01 10:39:45 -0400

    basic mongoose schemas

---
+++ Models/Stock.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const stockSchema = new mongoose.Schema({
+  ticker: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  coffee: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  }
+});
+
+//Export the model using the schema above
+module.exports = mongoose.model("stock", userSchema);
---
+++ Models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  balance: {
+    type: Number,
+    default: 5000
+  },
+  transactions: {
+    list: []
+  }
+});
+
+//Export the model using the schema above
+module.exports = mongoose.model("user", userSchema);
