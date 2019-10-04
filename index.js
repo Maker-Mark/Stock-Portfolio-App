94b486bfd6b2d18076e82e47d2705bc00d362b1c
Mark Goldstein <markgoldstein12@gmail.com>
2019-10-04 13:39:51 -0400

    route updates

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/db");
+//Establish the database connection
 connectDB();
+
 //Allow control origin, then call next middleware
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -17,10 +19,6 @@ app.use(function(req, res, next) {
 });
 //Allows us to accept body data
 app.use(express.json({ extended: false }));
-
-app.get("/", (req, res) => {
-  res.send("Hello from the backend!");
-});
 
 //Bring in user routes to the express app!
 app.use("/users", require("./routes/users"));
