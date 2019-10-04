7167dc36d4a4052e21f4eb46f37f43e556b40cf1
Mark Goldstein <markgoldstein12@gmail.com>
2019-10-04 14:42:56 -0400

    deploy test

--- index.js
+++ index.js
@@ -19,12 +19,11 @@ app.use(function(req, res, next) {
 });
 //Allows us to accept body data
 app.use(express.json({ extended: false }));
-app.get("/", (req, res) => {
-  res.send("Hello from the backend!");
-});
 //Bring in user routes to the express app!
 app.use("/users", require("./routes/users"));
 app.use("/stocks", require("./routes/stocks"));
-
+app.get("/", (req, res) => {
+  res.send("Hello from the backend!");
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Express server running on port ${PORT}`));
